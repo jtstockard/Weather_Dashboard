73746ddfccd5e5b1183b9fd68d1282fa1103ddc4
jtstockard <jtstockard92@gmail.com>
2021-04-21 02:05:28 -0500

    added psuedo code and other starting pages

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+/* PSUEDO CODE 
+
+1. GET API KEY
+*use api key from OpenWeather to weather access of new accuasitions
+*Use the three difreernt calls for current, 5-day, and UX index
+
+2. BOOTSTRAP
+* Use link and script for css and js
+* Fill in containers on html with css and html div classes
+
+3. JQUERY
+* use JSON to bring content to html from method placement
+* use ajax to get api clearence
+
+4. LOCALSTORAGE
+*Implement localstorage in the input selection to save console data
+ */
