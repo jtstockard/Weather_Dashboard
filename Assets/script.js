544913e3715f8a4f3b8bb24ef470f1e6bddec612
jtstockard <jtstockard92@gmail.com>
2021-04-22 01:11:25 -0500

    ajax for current viewed city,search btn fnctn

--- Assets/script.js
+++ Assets/script.js
@@ -22,5 +22,42 @@ const YOUR_API_KEY = "c2c522dc356d35787144fc3d640d1650";
 
 for (var i = 0; i < localStorage.length; i++) {
   var city = localStorage.getItem(i);
-  console.log(localStorage.getItem("City"));
+  //console.log(localStorage.getItem("City"));
+  var cityName = $(".list-group").addClass("list-group-item");
+
+  cityName.append("<li>" + city + "<li>");
 }
+
+var keyCount = 0;
+
+searchBtn.click(function () {
+  var searchInput = $(".searchInput").val();
+  var fiveDayUrl =
+    "api.openweathermap.org/data/2.5/forecast?q=" +
+    searchInput +
+    "&appid=" +
+    YOUR_API_KEY +
+    "&units=imperial";
+  var currentUrl =
+    "api.openweathermap.org/data/2.5/weather?q=" +
+    searchInput +
+    "&appid=" +
+    YOUR_API_KEY +
+    "&units=imperial";
+
+  if ((searchInput = "")) {
+    console.log(searchInput);
+  } else {
+    $.ajax({
+      url: currentUrl,
+      method: "GET",
+    }).then(function (response) {
+      var currentUV = currentTemp
+        .append("<p>" + "UV Index: " + response.value + "</p>")
+        .addClass("card-text");
+      currentUV.addClass("UV");
+      currentTemp.append(currentUV);
+      currentUV.append("UV Index: " + response.value);
+    });
+  }
+});
